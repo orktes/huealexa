036012fd60ca3788d5037e8af16f776154856160
Jaakko Lukkari <jaakko@unity3d.com>
2016-11-24 01:11:23 +0200

    Add comments to testscript

--- testscript.js
+++ testscript.js
@@ -1,12 +1,15 @@
 function getLights() {
+  // Call real hue bridge
   return JSON.parse(exec("curl 'http://10.0.1.3/api/3F430DA686/lights'"));
 }
 
 function getLight(id) {
+  // Call real hue bridge
   return JSON.parse(exec("curl 'http://10.0.1.3/api/3F430DA686/lights/" + id + "'"));
 }
 
 function setLightState(id, state) {
+  // Call real hue bridge
   return JSON.parse(exec("curl 'http://10.0.1.3/api/3F430DA686/lights/" + id + "/state' -X PUT --data-binary '" + JSON.stringify(state) + "'"));
 }
 
