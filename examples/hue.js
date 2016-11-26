5552c32728f50248d4ec399091ddd230e8c3f3c5
Jaakko Lukkari <jaakko@unity3d.com>
2016-11-26 17:56:02 +0200

    Use http module for hue example also

--- examples/hue.js
+++ examples/hue.js
@@ -1,19 +1,19 @@
-var exec = require('process').exec;
+var request = require('http').request;
 var registry = require('registry');
 
 registry._getLights = function () {
   // Call real hue bridge
-  return JSON.parse(exec("curl 'http://10.0.1.3/api/3F430DA686/lights'"));
+  return request("http://10.0.1.3/api/3F430DA686/lights");
 }
 
 registry._getLight = function (id) {
   // Call real hue bridge
-  return JSON.parse(exec("curl 'http://10.0.1.3/api/3F430DA686/lights/" + id + "'"));
+  return request("http://10.0.1.3/api/3F430DA686/lights/" + id);
 }
 
 registry._setLightState = function (id, state) {
   // Call real hue bridge
-  return JSON.parse(exec("curl 'http://10.0.1.3/api/3F430DA686/lights/" + id + "/state' -X PUT --data-binary '" + JSON.stringify(state) + "'"));
+  return request("http://10.0.1.3/api/3F430DA686/lights/" + id + "/state", 'PUT', state);
 }
 
 
