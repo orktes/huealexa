4655b4ab5bbc41bdd38555864662213b404f15b9
Jaakko Lukkari <jaakko.lukkari@gmail.com>
2016-12-20 16:29:49 +0200

    Remove debug code from example

--- examples/simple.js
+++ examples/simple.js
@@ -10,15 +10,3 @@ registry.addDevice(registry.createDevice('Second', registry.DimmableLamp, functi
   console.log('State changed - ' + JSON.stringify(state));
   return cb();
 }));
-/*
-// Try out a bash command
-console.log(exec("echo Script loaded"));
-
-var registry = require('registry');
-require('devices/tv/viera').discoverDevices(function (devices) {
-  if (devices.length > 0) {
-    registry.addDevice(devices[0].createDevice('TV'));
-    registry.addDevice(devices[0].createDevice('Apple TV', 'NRC_CHG_INPUT-ONOFF', 'NRC_TV-ONOFF'));
-  }
-});
-*/
